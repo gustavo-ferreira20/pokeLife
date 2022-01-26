b60b737a96ba175141489bd5578e1d498d8e6d3e
Gustavo Rodrigues <ferreirasouza20@hotmail.com>
2022-01-26 08:11:05 +0800

    Initial Commit

---
+++ pokeLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokeLife
+//
+//  Created by Gustavo rodrigues on 2022/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
